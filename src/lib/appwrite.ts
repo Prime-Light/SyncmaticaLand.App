909384999fa299e2ac7ee5ea12ec257bf17ae05c
MoYuan-CN <m56042764@163.com>
2026-02-20 23:08:35 +0800

    feat(appwrite): 初始化

---
+++ src/lib/appwrite.ts
@@ -0,0 +1,14 @@
+import { Client, Account, Databases } from "appwrite";
+
+let client = new Client().setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!).setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
+
+if (process.env.NODE_ENV === 'development') {
+    client = client.setDevKey(process.env.NEXT_PUBLIC_APPWRITE_DEVKEY!)
+} else {
+    /** @todo */
+}
+
+const account = new Account(client);
+const databases = new Databases(client);
+
+export { client, account, databases };
