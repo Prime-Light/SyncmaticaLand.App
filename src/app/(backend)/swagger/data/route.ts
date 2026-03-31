c594f11532301eda0f4b46438968c7713c3327d4
MoYuan-CN <m56042764@163.com>
2026-03-31 17:18:38 +0800

    fix: swagger data broken on prod

--- src/app/(backend)/swagger/data/route.ts
+++ src/app/(backend)/swagger/data/route.ts
@@ -1,6 +1,16 @@
 import { swaggerSpec } from "@/swagger/swagger";
 import { NextResponse } from "next/server";
+import swaggerSpecProduction from "./swagger.json";
 
 export async function GET() {
-    return NextResponse.json(swaggerSpec);
+    let resp;
+    switch (process.env.NODE_ENV) {
+        case "development":
+            resp = swaggerSpec;
+            break;
+        case "production":
+            resp = swaggerSpecProduction;
+            break;
+    }
+    return NextResponse.json(resp);
 }
